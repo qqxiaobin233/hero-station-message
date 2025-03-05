5816d3aacfdde4f239c14020bb5f4d6191b71c80
qqxiaobin <3254560365@qq.com>
2025-03-06 07:50:06 +0800

    英雄站后台管理系统

---
+++ src/login/index.js
@@ -0,0 +1,69 @@
+import { checkPhone, checkCode } from '@/utils/check.js'
+
+// document.querySelector('.btn').addEventListener('click', () => {
+//   const phone = document.querySelector('.login-form [name=mobile]').value
+//   const code = document.querySelector('.login-form [name=code]').value
+
+//   if (!checkPhone(phone)) {
+//     console.log('手机号长度必须是11位')
+//     return
+//   }
+
+//   if (!checkCode(code)) {
+//     console.log('验证码长度必须是6位')
+//     return
+//   }
+
+//   console.log('提交到服务器登录...')
+// })
+
+import 'bootstrap/dist/css/bootstrap.css'
+import './index.css'
+import './index.less'
+
+
+
+import myAxios from '@/utils/request.js'
+import { Myalert } from '@/utils/alert.js'
+document.querySelector('.btn').addEventListener('click', () => {
+  const phone = document.querySelector('.login-form [name=mobile]').value
+  const code = document.querySelector('.login-form [name=code]').value
+
+  if (!checkPhone(phone)) {
+    Myalert(false,'手机号长度必须是11位')
+    console.log('手机号长度必须是11位')
+    return
+  }
+
+  if (!checkCode(code)) {ox
+    Myalert(false,'验证码长度必须是6位')
+    console.log('验证码长度必须是6位')
+    return
+  }
+
+  myAxios({
+    method:'post',
+    url:'/v1_0/authorizations',
+    data:{
+      mobile:phone,
+      code
+    }
+  }).then(result=>{
+    Myalert(true,'登录成功')
+    localStorage.setItem('token',result.data.token)
+    location.href = '../content/index.html'
+  }).catch(err=>{
+    Myalert(false,'登录失败')
+  })
+  console.log('提交到服务器登录...')
+})
+
+// if(process.env.NODE_ENV === 'production'){
+//   console.log = function(){}
+// }
+// console.log('开发模式下好用，生产模式下失效')
+
+
+// import mytest from '@/utils/request.js'
+// console.log(mytest)
+
---
+++ webpack.config.js
@@ -0,0 +1,105 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const webpack = require('webpack')
+
+const config ={
+  mode:'development',
+  // entry:path.resolve(__dirname,'./src/login/index.js'),
+  entry:{
+    'login':path.resolve(__dirname,'src/login/index.js'),
+    'content':path.resolve(__dirname,'src/content/index.js'),
+    'publish':path.resolve(__dirname,'src/publish/index.js')
+  
+  },
+  output: {
+      path: path.resolve(__dirname,'dist'),
+      filename: './[name]/index.js',
+      clean:true //生成打包后的内容之前，清空输出的目录
+  },
+  //插件
+  plugins:[
+      new MiniCssExtractPlugin({
+        filename:'./[name]/index.css'
+      }),
+      new HtmlWebpackPlugin({
+          template:path.resolve(__dirname,'public/login.html'),
+          filename:path.resolve(__dirname,'dist/login/index.html'),
+          useCdn:process.env.NODE_ENV === 'production',//生产模式下使用cdn引用的地址
+          chunks:['login'] //引入哪些打包后的模块（和entry的key一致）
+      }),
+      new HtmlWebpackPlugin({
+        template:path.resolve(__dirname,'public/content.html'),
+        filename:path.resolve(__dirname,'dist/content/index.html'),
+        useCdn:process.env.NODE_ENV === 'production',//生产模式下使用cdn引用的地址
+        chunks:['content']
+      }),
+      new HtmlWebpackPlugin({
+        template:path.resolve(__dirname,'public/publish.html'),
+        filename:path.resolve(__dirname,'dist/publish/index.html'),
+        useCdn:process.env.NODE_ENV === 'production',//生产模式下使用cdn引用的地址
+        chunks:['publish'] //引入哪些打包后的模块（和entry的key一致）
+    }),
+      new webpack.DefinePlugin({
+          'process.env.NODE_ENV':JSON.stringify(process.env.NODE_ENV)
+      })
+  ],
+  //加载器
+  module:{
+      rules: [
+          {
+            test: /\.css$/i,
+            use: [process.env.NODE_ENV === 'development'?'style-loader': MiniCssExtractPlugin.loader, "css-loader"],
+          },
+          {
+              test: /\.less$/i,
+              use: [
+                // compiles Less to CSS
+                process.env.NODE_ENV === 'development'?'style-loader': MiniCssExtractPlugin.loader,
+                'css-loader',
+                'less-loader',
+              ],
+          },
+          {
+              test:/\.(png|jpg|gif|jpeg)$/i,
+              type:'asset',
+              generator:{
+                  filename:'assets/[hash][ext][query]'
+              }
+          },
+        ],
+  },
+  //优化
+  optimization: {
+      minimizer: [
+        // 在 webpack@5 中，你可以使用 `...` 语法来扩展现有的 minimizer（即 `terser-webpack-plugin`），将下一行取消注释
+        `...`,
+        new CssMinimizerPlugin(),
+      ],
+    },
+  //解析
+  resolve:{
+    alias:{
+      '@':path.resolve(__dirname,'src')
+    }
+  }
+}
+
+if(process.env.NODE_ENV === 'development'){
+  config.devtool = 'inline-source-map'
+}
+
+//防止import的包被异常处理
+if(process.env.NODE_ENV === 'production'){
+  config.externals={
+    //key:import from 语句后面的字符串
+    //value:留在原地的全局变量（最好和cdn在全局暴露的变量一致）
+    'bootstrap/dist/css/bootstrap.min.css':'bootstrap',
+    'axios':'axios',
+    'form-serialize' : 'serialize',
+    '@wangeditor/editor' : 'wangEditor'
+  }
+}
+
+module.exports = config
